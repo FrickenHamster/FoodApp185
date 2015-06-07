d833e6ae72956b6c99341502364c8d7fa6c64773
FamiliarStranger <dario-55@hotmail.com>
2015-06-06 17:28:00 -0700

    Added History activity and layouts

---
+++ app/src/main/java/edu/ucsb/cs/cs185/frickenhamster/food/history/HistoryAdapter.java
@@ -0,0 +1,84 @@
+package edu.ucsb.cs.cs185.frickenhamster.food.history;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import edu.ucsb.cs.cs185.frickenhamster.food.R;
+
+/**
+ * Created by Dario on 6/62015.
+ */
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+    private List<String> myDataset;
+    private HistoryActivity activity;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView myFoodTypeView;
+        public TextView myRestaurantNameView;
+        public TextView myDateView;
+        public Context context;
+        final HistoryActivity historyActivity;
+
+        public ViewHolder(HistoryActivity activity, View v, Context context) {
+            super(v);
+            myFoodTypeView = (TextView) v.findViewById(R.id.food_type);
+            myRestaurantNameView = (TextView) v.findViewById(R.id.restaurant_name);
+            myDateView = (TextView) v.findViewById(R.id.date);
+
+            this.context = context;
+            this.historyActivity = activity;
+
+            /*
+            myTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //openFullScreenPhoto(cardactivity, currentPhoto);
+                }
+            });
+            */
+        }
+    }
+
+    public HistoryAdapter(HistoryActivity activity, List<String> myDataset) {
+        this.myDataset = myDataset;
+        this.activity = activity;
+    }
+
+    public HistoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.history_card_layout, parent, false);
+
+        ViewHolder vh = new ViewHolder(activity, v, parent.getContext());
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+
+        holder.myFoodTypeView.setText(myDataset.get(position));
+        holder.myRestaurantNameView.setText("Restaurant name goes here");
+        holder.myDateView.setText("Date goes here");
+        //Picasso.with(holder.context).load(myDataset.get(position)).resize(800, 400).centerCrop().into(holder.myImageView);
+
+        //holder.currentPhoto = myDataset.get(position);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return myDataset.size();
+    }
+
+    public void deleteAll() {
+
+    }
+}
